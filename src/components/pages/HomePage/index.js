ca3c25178f9f7d3eb59b4a4e9bffe9c2ef086e4b
Sebastian MacDonald <optissimum@gmail.com>
2017-05-15 20:57:08 -0400

    Update homepage

--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -5,7 +5,7 @@ import { PageTemplate, Header, Hero, Footer, FeatureList } from 'components'
 
 const HomePage = () => {
   return (
-    <PageTemplate header={<Header />} hero={<Hero />} footer={<Footer />}>
+    <PageTemplate header={<Header />} footer={<Footer />}>
       <FeatureList />
     </PageTemplate>
   )
