9c52cf6e521483f27c87d7d58cdd050ce5839225
Sebastian MacDonald <optissimum@gmail.com>
2017-05-17 23:11:26 -0400

    Fix Org page with correct internal linking

--- src/components/organisms/OrganizationList/index.js
+++ src/components/organisms/OrganizationList/index.js
@@ -38,7 +38,7 @@ const OrganizationList = ({ ...props }) => (
     <StyledHeading>Organizations</StyledHeading>
     <Description>
       Places in the local community we are currently working with:<br />
-      <Link href="/organization-registry">Register your organization today!</Link>
+    <Link to="/orgregistry">Register your organization today!</Link>
     </Description>
     <Grid>
       <StyledFeature
